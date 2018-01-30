c45664629897d95a6aee2f0f5373cadb155ee7c8
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-01-30 16:36:26 +0530

    Create naveentask2.c

---
+++ naveentask2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+printf("Enter the a value");
+scanf("%d",&a);
+if(a%2==0)
+{
+    printf("Even");
+}
+else
+{
+    printf("Odd");
+}
+getch();
+}
