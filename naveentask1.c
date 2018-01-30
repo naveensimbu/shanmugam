4b01f249d30ff8c1b155970ab99155f947a428c5
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-01-30 16:29:34 +0530

    Create naveentask1.c

---
+++ naveentask1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter the a value");
+scanf("%d,&a);
+if(a>0)
+{
+printf("positive");
+}
+else if(a<O)
+{
+printf("Negative");
+}
+else
+{
+printf("zero");
+}
+getch();
+}
