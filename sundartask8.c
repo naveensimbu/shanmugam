9b2c7b4f37f39ba3186299a92be7b5bda8ee2767
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-02-09 15:56:04 +0530

    Create sundartask8.c

---
+++ sundartask8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+	int a[10],b,i,temp;
+	scanf("%d",&b);
+	for(i=0;i<b;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<b;i++)
+	{
+		printf("%d   %d\n",a[i],i);
+	}
+	
+	return 0;
+}
