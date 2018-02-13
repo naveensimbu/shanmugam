8128bf283fd5733168da2cdde7147b05c89f2c17
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-02-13 16:06:48 +0530

    Create rajatask4.c

---
+++ rajatask4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+
+{
+	int m,n,ref;
+	scanf("%d",&m);
+	scanf("%d",&n);
+	scanf("%d",&ref);
+	if(ref>=m && ref<=n)
+	printf("yes");
+	else
+	printf("no");
+	return 0;
+
+}
