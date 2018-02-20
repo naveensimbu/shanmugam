1185479ca10732a5e67fde2b5871502589d21d1e
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-02-20 16:30:48 +0530

    Update ns1.c

--- ns1.c
+++ ns1.c
@@ -6,16 +6,16 @@ int main(void)
 	scanf("%d",&num);
 	while(num!=0)
 	{
-		rem=num%10;
-		rev=(rev*10)+rem;
-		num=num/10;
+		rem=num%20;
+		rev=(rev*20)+rem;
+		num=num/20;
 	}
 	num=rev;
 	while(num!=0)
 	{
-		rem=num%10;
+		rem=num%20;
 		printf("%d ",rem);
-		num=num/10;
+		num=num/20;
 	}
 	return 0;
 
