e37b87eb72b4b8320ed154f1f2afef99ec5accf4
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-02-22 15:55:03 +0530

    Create ns5.c

---
+++ ns5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int n,m,temp;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	temp=n*m;
+	temp=temp%2;
+	if(temp==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	
+	return 0;
+}
