3679ee187df2470fdf76f6715345584fc7805a94
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-01 16:30:44 +0530

    Create aym5.c

---
+++ aym5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(void) {
+	char a[10];
+	int n,i,b,c;
+	scanf("%s",a);
+	n=strlen(a);
+	b=n%2;
+	c=n/2;
+	if(b==0)
+	{
+		 a[c]='*';
+		 a[c-1]='*';
+		 printf("%s",a);
+	}
+	else
+	{
+		a[c]='*';
+		printf("%s",a);
+	}
+	return 0;
+}
