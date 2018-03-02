a70473acbb9e883c0dccb290078da29ffbd4dcf2
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-02 14:20:38 +0530

    Create ranbir1.c

---
+++ ranbir1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+	int a,b,ans;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a>b)
+	{
+		ans=a-b;
+		printf("%d",ans);
+	}
+	else
+	{
+		ans=b-a;
+		printf("%d",ans);
+	}
+
+	return 0;
+}
