f8f8d676541c52b616ed707165b8b6a4188b448f
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-02 14:08:26 +0530

    Create aym9.c

---
+++ aym9.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  int num1,num2,product,count=0,i;
+  scanf("%d%d",&m,&n);
+  product=m*n;
+  for(i=0;i<=product;i++)
+  {  
+   if(product==i*i)
+   {
+    count=1;
+   	
+   }
+  }
+if(count==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+
+}
