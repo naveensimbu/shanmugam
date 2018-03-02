acd3cd51242d2d0b0f3271bb95a9d8c918ce4c89
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-02 14:35:38 +0530

    Create ranbir5.c

---
+++ ranbir5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[10];
+	int n,i;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i=i+2)
+	{
+		printf("%c",a[i]);
+	}
+	printf("\t");
+	for(i=1;i<n;i=i+2)
+	{
+		printf("%c",a[i]);
+	}
+	
+	return 0;
+}
