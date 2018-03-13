d2856cc0def858fff2cdb036081c27cf41230cc8
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-13 16:02:41 +0530

    Create ccv7.c

---
+++ ccv7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int n,rev=0,temp;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+	temp=n%10;
+	rev=rev*10+temp;
+	temp=temp/10;
+	
+	}
+	printf("%d",rev);
+  return 0;
+}
