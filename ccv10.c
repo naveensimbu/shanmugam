60221fd171caee5e79199dc8a168aa162cb89c1d
naveensimbu <35957601+naveensimbu@users.noreply.github.com>
2018-03-13 16:10:22 +0530

    Create ccv10.c

---
+++ ccv10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,remainder,product=1;
+scanf("%d",&n);
+while(n!=0)
+{
+remainder=n%10;
+product=product*remainder;
+n=n%10;
+}
+printf("\n %d",product);
+return 0;
+}
